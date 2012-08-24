8831939ce1dbaac0235419953f8878ed88b95e78
Hendrik Liebau <mail@hendrik-liebau.de>
2012-08-24 14:58:44 +0200

    cleaned up home controller

--- src/main/java/de/hliebau/tracktivity/presentation/HomeController.java
+++ src/main/java/de/hliebau/tracktivity/presentation/HomeController.java
@@ -1,11 +1,13 @@
 package de.hliebau.tracktivity.presentation;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import de.hliebau.tracktivity.domain.Track;
 import de.hliebau.tracktivity.service.TrackService;
 
 @Controller
@@ -16,8 +18,11 @@ public class HomeController {
 
 	@RequestMapping({ "/" })
 	public String showHomePage(Map<String, Object> model) {
-		model.put("headline", "Tracks");
-		model.put("tracks", trackService.getRecentTracks(10));
+		long start = System.currentTimeMillis();
+		List<Track> tracks = trackService.getRecentTracks(50);
+		long end = System.currentTimeMillis();
+		model.put("fetchTime", end - start);
+		model.put("tracks", tracks);
 		return "home";
 	}
 
