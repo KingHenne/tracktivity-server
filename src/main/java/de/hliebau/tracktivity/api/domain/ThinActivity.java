64d401777cf199bd5c35527640d6a3123a3eca3a
Hendrik Liebau <mail@hendrik-liebau.de>
2012-09-23 13:59:34 +0200

    thinned out ThinActivity to only include the ID

--- src/main/java/de/hliebau/tracktivity/api/domain/ThinActivity.java
+++ src/main/java/de/hliebau/tracktivity/api/domain/ThinActivity.java
@@ -1,14 +1,14 @@
 package de.hliebau.tracktivity.api.domain;
 
-import java.util.Date;
-
 import javax.xml.bind.annotation.XmlAttribute;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonRootName;
 
 import de.hliebau.tracktivity.domain.Activity;
 
 @XmlRootElement(name = "activity")
+@JsonRootName("activity")
 public class ThinActivity {
 
 	private Activity activity;
@@ -20,25 +20,25 @@ public class ThinActivity {
 		this.activity = activity;
 	}
 
-	@XmlElement
-	public Date getCreated() {
-		return activity.getCreated();
-	}
+	// @XmlElement
+	// public Date getCreated() {
+	// return activity.getCreated();
+	// }
 
 	@XmlAttribute
 	public Long getId() {
 		return activity.getId();
 	}
 
-	@XmlElement
-	public String getName() {
-		return activity.getName();
-	}
+	// @XmlElement
+	// public String getName() {
+	// return activity.getName();
+	// }
 
-	@XmlElement
-	public String getRef() {
-		return String.format("/activities/%d", activity.getId());
-	}
+	// @XmlElement
+	// public String getRef() {
+	// return String.format("/activities/%d", activity.getId());
+	// }
 
 	public void setActivity(Activity activity) {
 		this.activity = activity;
